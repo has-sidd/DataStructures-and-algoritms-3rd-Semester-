5820895be292185bffe53a138feee87eaba0ec6e
Hassaan Siddiqui <59255860+has-sidd@users.noreply.github.com>
2021-11-23 01:56:21 +0500

    Add files via upload

---
+++ Traversal/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int num[5] = { 5, 7, 6, 9, 4 };
+	for (int i = 0; i <= 4; i++)
+	{
+		cout << num[i] << endl;
+	}
+
+	_getch();
+}
