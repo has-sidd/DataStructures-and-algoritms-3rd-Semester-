b887dfd893f53546b7fab7e6e6ce7cda7a2196fc
Hassaan Siddiqui <59255860+has-sidd@users.noreply.github.com>
2021-11-23 01:57:06 +0500

    Add files via upload

---
+++ Solve Postfix/Source.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main()
+{
+	stack<int> Stack;
+	string p;
+	int i = 0;
+	int a, b;
+	p = { '5', '6', '2', '+', '*', '9', '4', '/', '-', ')' };
+	while (p[i] != ')')
+		{
+			if (p[i] != '+' || '-' || '*' || '/')
+			{
+				Stack.push(p[i]);
+			}
+			else
+			{
+				switch (p[i])
+				{
+				case '+':
+					a = int(Stack.top());
+					Stack.pop();
+					b = int(Stack.top()); 
+					Stack.pop();
+					Stack.push(a + b);
+					
+				case '-':
+					a = int(Stack.top());
+					Stack.pop();
+					b = int(Stack.top());
+					Stack.pop();
+					Stack.push(a - b);
+					
+				case '*':
+					a = int(Stack.top());
+					Stack.pop(); 
+					b = int(Stack.top());
+					Stack.pop();
+					Stack.push(a * b);
+					
+				case '/':
+					a = int(Stack.top());
+					Stack.pop(); 
+					b = int(Stack.top());
+					Stack.pop();
+					Stack.push(a / b);
+					
+			}
+			i++;
+		}
+	}
+	cout << "Result: " << Stack.top() << endl;
+	_getch();
+}
